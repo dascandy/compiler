729bcf9130fc4e37589376f7f3cd6eb309bbb3c9
Peter Bindels <peter.bindels@tomtom.com>
2017-11-20 17:18:42 +0100

    added grammar

--- src/parser.h
+++ src/parser.h
@@ -25,4 +25,25 @@ public:
   bool operator!=(const sentinel&) { return cur <= str.size(); }
 };
 
+translationunit ::= { declaration }
+declaration ::= [ template_decl ] ( var_decl | func_decl | class_decl | enum_decl | using_decl)
+class_decl ::= 'class' <name> [ inheritance_decl ] '{' { ( accessor | declaration ) } '}' ';'
+var_decl ::= type_decl <name> { ',' <name> } ';'
+func_decl ::= type_decl <name> '(' arg_decl { ',' arg_decl } '{' { statement } '}'
+template_decl ::= 'template' '<' template_arg { ',' template_arg } '>'
+
+using_decl ::= 'using' <name> '=' type_decl ';'
+
+statement ::= declaration
+            | 
+
+template_arg ::= ( 'typename' | 'class' ) [ <name> ] [ '=' type_decl ]
+               | integral_type [ <name> ] [ '=' default_value ]
+
+
+float_type ::= 'float' | [ 'long' ] 'double'
+integral_type ::= [ 'unsigned' | 'signed' ] 'int' | 'short' | 'char' | 'long' | 'long' 'long'
+inheritance_decl ::= ':' access_specifier <name> { ',' access_specifier <name> } 
+accessor ::= access_specifier ':'
+access_specifier ::= 'public' | 'private' | 'protected'
 
